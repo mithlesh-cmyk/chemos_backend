0ea53ff6fce2e9f78f36a52bbd1886d8cc8b4920
amitkumar-cell <sidheresearchteam@gmail.com>
2026-08-13 16:41:04 +0530

    new things has been added

--- src/main/java/chemos/chem_os/dto/VesselStockStatsResponse.java
+++ src/main/java/chemos/chem_os/dto/VesselStockStatsResponse.java
@@ -1,5 +1,9 @@
 package chemos.chem_os.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public record VesselStockStatsResponse(
         String vesselName,
         String product,
@@ -12,6 +16,10 @@ public record VesselStockStatsResponse(
         Double incomingSold,
         Double incomingUnsoldClosing,
         Double totalStock,
-        String companyName
+        String companyName,
+        BigDecimal marketPrice,
+        BigDecimal replacementCost,
+        LocalDateTime date,
+        LocalDate vesselDate
 ) {
 }
